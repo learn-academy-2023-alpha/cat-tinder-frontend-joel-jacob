9a4d2ca4d65e6ecf48b47c6a86515c98da49857a
JoelCarr619 <JoelCarr619@gmail.com>
2023-04-07 13:21:08 -0700

    updated the update to the update of the code

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,9 +1,21 @@
-import { render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import Home from "./Home";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<Home />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    render(<Home />, div);
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>,
+      div
+    );
+
+    screen.logTestingPlaygroundURL();
+    const home = screen.getByRole("heading", {
+      name: /welcome everyone to the greatest show you never saw, yet!/i,
+    });
+    expect(home).toBeInTheDocument();
   });
 });
